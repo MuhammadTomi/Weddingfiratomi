cd35eadd5312d245351723cf04b2496301ab21ad
Muhammad Tomi <83509289+MuhammadTomi@users.noreply.github.com>
2025-05-20 23:14:44 +0700

    Update data.js

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -28,7 +28,7 @@ export const data = {
             day: 'Selasa',
             hours: {
                 start: '11.00',
-                finish: 'Selesai'
+                finish: 'Selesai',
         reception: {
             year: '2025',
             month: 'Mei',
