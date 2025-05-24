bf296a2d5816040ccc746e992c05ecbd3e726f73
Muhammad Tomi <83509289+MuhammadTomi@users.noreply.github.com>
2025-05-24 16:45:23 +0700

    Update data.js

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -22,10 +22,10 @@ export const data = {
 
     time: {
         marriage: {
-            year: '2025',
-            month: 'April',
-            date: '22',
-            day: 'Selasa',
+            year: '',
+            month: '',
+            date: '',
+            day: 'Selasa 22 April 2025',
             hours: {
                 start: '11.00',
                 finish: 'Selesai'
